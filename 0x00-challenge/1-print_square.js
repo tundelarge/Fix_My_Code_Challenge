817d9a42c5282c3d679975eeaa56b72629a142b9
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-02-18 09:00:30 +0100

    Update 1-print_square.js

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,14 +6,14 @@
 */
 
 
-if (process.argv.length <= 2) {
+if (process.argv.length <= 4) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js 4<size>\n");
     process.stderr.write("Example: ./1-print_square.js 10\n");
     process.exit(1)
 }
 
-size = parseInt(process.argv[2], 10)
+size = parseInt(process.argv[4], 10)
 
 for (let i = 0 ; i < size ; i++) {
     for (let j = 0 ; j < size ; j++) {
